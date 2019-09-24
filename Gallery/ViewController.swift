c5799174d2bd6695cb5c90d9a61a876f48d7eddd
Piti Irawan <piti.irawan@gmail.com>
2019-09-24 11:13:52 -0700

    Initial Commit

---
+++ Gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Piti Irawan on 2019/09/24.
+//  Copyright © 2019 Piti Irawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
