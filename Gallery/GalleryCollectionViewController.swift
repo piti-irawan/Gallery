8a5cc82a1de6ead408b48ce53f9cfe545e254f74
Piti Irawan <piti.irawan@gmail.com>
2019-09-24 17:17:59 -0700

    Drag and drop seem to work.

---
+++ Gallery/GalleryCollectionViewController.swift
@@ -0,0 +1,174 @@
+//
+//  GalleryCollectionViewController.swift
+//  Gallery
+//
+//  Created by Piti Irawan on 2019/09/24.
+//  Copyright © 2019 Piti Irawan. All rights reserved.
+//
+
+import UIKit
+
+class GalleryCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDragDelegate, UICollectionViewDropDelegate {
+    private static let cellWidth = 200.0
+    private var cellData = [(url: URL, aspectRatio: Double)]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Do any additional setup after loading the view.
+        //galleryCollectionView.dropDelegate = self
+        collectionView.dragDelegate = self
+        collectionView.dropDelegate = self
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cellData.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GalleryCell", for: indexPath)
+        if let galleryCell = cell as? GalleryCollectionViewCell {
+            let data = cellData[indexPath.item]
+            galleryCell.spinner.startAnimating()
+            galleryCell.imageView.image = nil
+            DispatchQueue.global(qos: .userInitiated).async {
+                let urlContents = try? Data(contentsOf: data.url)
+                if let imageData = urlContents {
+                    DispatchQueue.main.async {
+                        galleryCell.imageView.image = UIImage(data: imageData)
+                        galleryCell.spinner.stopAnimating()
+                    }
+                }
+            }
+        }
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+    
+    // MARK: UICollectionViewDelegateFlowLayout
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        return CGSize(width: GalleryCollectionViewController.cellWidth, height: GalleryCollectionViewController.cellWidth / cellData[indexPath.item].aspectRatio)
+    }
+    
+    // MARK: UICollectionViewDragDelegate
+    
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        session.localContext = collectionView
+        return dragItems(at: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, itemsForAddingTo session: UIDragSession, at indexPath: IndexPath, point: CGPoint) -> [UIDragItem] {
+        return dragItems(at: indexPath)
+    }
+    
+    private func dragItems(at indexPath: IndexPath) -> [UIDragItem] {
+        let data = cellData[indexPath.item]
+        //if let data = (collectionView.cellForItem(at: indexPath) as? GalleryCollectionViewCell)?.data {
+        let itemProvider = NSItemProvider()
+        itemProvider.registerObject(NSURL(), visibility: .all)
+        itemProvider.registerObject(UIImage(), visibility: .all)
+        let dragItem = UIDragItem(itemProvider: itemProvider)
+        dragItem.localObject = data
+        return [dragItem]
+    }
+    
+    // MARK: UICollectionViewDropDelegate
+    
+    func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSURL.self) && session.canLoadObjects(ofClass: UIImage.self)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+        let isSelf = (session.localDragSession?.localContext as? UICollectionView) == collectionView
+        return UICollectionViewDropProposal(operation: isSelf ? .move : .copy, intent: .insertAtDestinationIndexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        let destinationIndexPath = coordinator.destinationIndexPath ?? IndexPath(item: 0, section: 0)
+        for item in coordinator.items {
+            if let sourceIndexPath = item.sourceIndexPath {
+                if let data = item.dragItem.localObject as? (URL, Double) {
+                    collectionView.performBatchUpdates({
+                        cellData.remove(at: sourceIndexPath.item)
+                        cellData.insert(data, at: destinationIndexPath.item)
+                        collectionView.deleteItems(at: [sourceIndexPath])
+                        collectionView.insertItems(at: [destinationIndexPath])
+                    })
+                    coordinator.drop(item.dragItem, toItemAt: destinationIndexPath)
+                }
+            } else {
+                var ratio: CGFloat?
+                item.dragItem.itemProvider.loadObject(ofClass: UIImage.self) { (provider, error) in
+                    if let image = provider as? UIImage {
+                        ratio = image.size.width / image.size.height
+                    }
+                }
+                item.dragItem.itemProvider.loadObject(ofClass: NSURL.self) { [weak self] (provider, error) in
+                    if let aspectRatio = ratio {
+                        DispatchQueue.main.async {
+                            let placeholderContext = coordinator.drop(item.dragItem, to: UICollectionViewDropPlaceholder(insertionIndexPath: destinationIndexPath, reuseIdentifier: "DropPlaceholderCell"))
+                            if let url = provider as? URL {
+                                placeholderContext.commitInsertion(dataSourceUpdates: { insertionIndexPath in
+                                    self?.cellData.insert((url.imageURL, Double(aspectRatio)), at: insertionIndexPath.item)
+                                })
+                            } else {
+                                placeholderContext.deletePlaceholder()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
