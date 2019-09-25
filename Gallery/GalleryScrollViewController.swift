3d5d8aeff889de2ba58ecb43d24e673a39036abd
Piti Irawan <piti.irawan@gmail.com>
2019-09-25 15:21:40 -0700

    Fixed scroll view zoom.

--- Gallery/GalleryScrollViewController.swift
+++ Gallery/GalleryScrollViewController.swift
@@ -10,32 +10,46 @@ import UIKit
 
 class GalleryScrollViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var scrollViewWidth: NSLayoutConstraint!
-    @IBOutlet weak var scrollViewHeight: NSLayoutConstraint!
-    @IBOutlet weak var viewForZooming: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewTrailingConstraint: NSLayoutConstraint!
     var image = UIImage()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.delegate = self
         scrollView.minimumZoomScale = 0.1
-        scrollView.maximumZoomScale = 5.0
+        scrollView.maximumZoomScale = 1.0
         scrollView.zoomScale = 1.0
-        viewForZooming.image = image
-        viewForZooming.frame = CGRect(origin: CGPoint.zero, size: image.size)
+        imageView.image = image
+        imageView.frame = CGRect(origin: CGPoint.zero, size: image.size)
         scrollView.contentSize = image.size
-        scrollViewWidth.constant = image.size.width
-        scrollViewHeight.constant = image.size.height
     }
 
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        let widthScale = view.bounds.size.width / imageView.bounds.width
+        let heightScale = view.bounds.size.height / imageView.bounds.height
+        let minScale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = minScale
+        scrollView.zoomScale = minScale
+    }
+    
     // MARK: - UIScrollViewDelegate
 
     func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        scrollViewWidth.constant = scrollView.contentSize.width
-        scrollViewHeight.constant = scrollView.contentSize.height
+        let yOffset = max(0, (view.bounds.size.height - imageView.frame.height) / 2)
+        imageViewTopConstraint.constant = yOffset
+        imageViewBottomConstraint.constant = yOffset
+        let xOffset = max(0, (view.bounds.size.width - imageView.frame.width) / 2)
+        imageViewLeadingConstraint.constant = xOffset
+        imageViewTrailingConstraint.constant = xOffset
+        view.layoutIfNeeded()
     }
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return viewForZooming
+        return imageView
     }
 }
